9a6387c944e47a12b058e9d5739331bc1504099d
khadijaumud <kh.umud07@gmail.com>
2025-12-01 08:53:04 +0000

    .

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints a specific message using printf.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
