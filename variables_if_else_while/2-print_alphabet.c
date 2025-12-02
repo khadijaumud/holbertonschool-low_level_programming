9393d0d5dd7bcd0a30d3da83e457e2fb24abbece
khadijaumud <kh.umud07@gmail.com>
2025-12-02 15:34:34 +0000

    .

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 /**
- *main-prints the alphabet in lowercase
+ * main - prints the alphabet in lowercase
  *
- *Return: Always 0
+ * Return: Always 0 (Success)
  */
- 
-int main (void)
+int main(void)
 {
 	char c = 'a';
 
-	while (c<= 'z')
-    {
-		putchar(c)
+	while (c <= 'z')
+	{
+		putchar(c);
 		c++;
 	}
 	putchar('\n');
