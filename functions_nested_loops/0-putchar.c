e37424307be427bdb3023a2086da5abf46e6bf91
khadijaumud <kh.umud07@gmail.com>
2025-12-04 17:49:01 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-    printf("_putchar\n");
+    char text[] = "_putchar\n";
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        _putchar(text[i]);
+    }
+
     return (0);
 }
