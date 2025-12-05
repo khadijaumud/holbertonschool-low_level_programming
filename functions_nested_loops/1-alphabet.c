d02ff870d4c449a2cb823762f5ca495cbad8de47
khadijaumud <kh.umud07@gmail.com>
2025-12-05 13:18:46 +0400

    .

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,14 +4,15 @@
 /**
  * print_alphabet - prints the lowercase alphabet followed by a new line
  */
+
 void print_alphabet(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
