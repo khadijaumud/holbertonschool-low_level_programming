4c7502c976350a90f4f4a1769f51d25d62c03acf
khadijaumud <kh.umud07@gmail.com>
2025-12-14 11:43:31 +0000

    fixed betty problem

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,6 @@ char *cap_string(char *str)
 
 	i = 0;
 
-	
 	if (str[i] >= 'a' && str[i] <= 'z')
 		str[i] = str[i] - 32;
 
@@ -20,7 +19,6 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		
 		if ((str[i - 1] == ' '  || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 		     str[i - 1] == ','  || str[i - 1] == ';'  || str[i - 1] == '.'  ||
 		     str[i - 1] == '!'  || str[i - 1] == '?'  || str[i - 1] == '"'  ||
