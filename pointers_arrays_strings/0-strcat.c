f93a65bf19c79fc1c61b022184044c5c1e6f9cfb
khadijaumud <kh.umud07@gmail.com>
2025-12-14 08:41:58 +0000

    done 1st task

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	/* find end of dest */
+	while (dest[i] != '\0')
+		i++;
+
+	/* append src to dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
