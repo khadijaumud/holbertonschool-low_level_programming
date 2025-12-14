a1a764c7e8bdc75639735a0c5e8b52486b6136fb
khadijaumud <kh.umud07@gmail.com>
2025-12-14 09:38:12 +0000

    2nd task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,7 +11,8 @@
  char *_strcpy(char *dest, char *src);
  char *_strcat(char *dest, char *src);
  char *_strncat(char *dest, char *src, int n);
- 
+ har *_strncpy(char *dest, char *src, int n);
+
 
 
 
