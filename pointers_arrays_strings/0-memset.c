baf6447a6a274a930a3fd9c60ed447f5932be5b5
khadijaumud <kh.umud07@gmail.com>
2025-12-14 13:06:31 +0000

    .

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset - fills the first n bytes of memory pointed to by s with byte b
+ * @s: pointer to the memory area
+ * @b: byte value to fill
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+
+    return s;
+}
