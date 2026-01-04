799fbabe058246e24834831a38d77bc2c7d441e8
khadijaumud <kh.umud07@gmail.com>
2026-01-04 17:47:49 +0000

    fix file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,4 +2,12 @@
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: function pointer
+ */
 void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
