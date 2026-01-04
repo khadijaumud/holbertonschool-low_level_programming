0353cf373f28e8eee3ea051eb093bd968525b847
khadijaumud <kh.umud07@gmail.com>
2026-01-04 17:42:54 +0000

    1st task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to be printed
+ * @f: pointer to the function that prints the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
