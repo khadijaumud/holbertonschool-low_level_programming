9b026c6104cbb94aec726ee4be197edbf502b8a1
khadijaumud <kh.umud07@gmail.com>
2026-01-04 17:52:10 +0000

    .

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,11 +3,9 @@
 
 #include <stddef.h>
 
-/**
- * print_name - prints a name using a function pointer
- * @name: name to print
- * @f: function pointer
- */
-void print_name(char *name, void (*f)(char *));
 
-#endif /* FUNCTION_POINTERS_H */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif 
+
