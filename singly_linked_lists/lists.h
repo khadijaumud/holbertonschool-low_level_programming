64d50c00e3c2fc5669076a9c08a96af43a09ffaa
khadijaumud <kh.umud07@gmail.com>
2026-01-12 18:45:54 +0000

    .

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> 
+#include <stddef.h>
 
 typedef struct list_s
 {
@@ -13,5 +13,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 
-#endif 
+#endif
 
