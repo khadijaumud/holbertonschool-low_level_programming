f72db1fba676ee29eda7c66eeffacee82fb56add
khadijaumud <kh.umud07@gmail.com>
2026-01-18 18:17:11 +0000

    fix betty style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to pointer to the head of the list
@@ -12,30 +11,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int i = 0;
+	list_t *new_node;
+	unsigned int i = 0;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    /* manual string length (strlen istifadə ETMİRİK) */
-    while (str[i] != '\0')
-        i++;
+	while (str[i] != '\0')
+		i++;
 
-    new_node->len = i;
-    new_node->next = *head;
-    *head = new_node;
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
