69f1b364f56a052ed28b811fa70cc73f4e64699e
khadijaumud <kh.umud07@gmail.com>
2026-01-18 18:12:06 +0000

    add 2-add_node.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,26 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
-/**
- * struct list_s - singly linked list node
- * @str: string stored in the node
- * @len: length of the string
- * @next: points to the next node in the list
- *
- * Description: This struct defines a node of a singly linked list.
- * Each node stores a string, its length, and a pointer to the next node.
- */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-/* Function prototype */
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
