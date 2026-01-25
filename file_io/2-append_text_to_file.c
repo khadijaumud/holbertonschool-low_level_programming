ab5b3e6250bc26a4d683c8b73078a009b4f03635
khadijaumud <kh.umud07@gmail.com>
2026-01-25 09:41:20 +0000

    fix betty style

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * append_text_to_file - appends text at the end of a file
@@ -10,34 +10,35 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t w;
-    int len = 0;
+	int fd;
+	ssize_t w;
+	int len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content == NULL)
-    {
-        close(fd);
-        return (1);
-    }
-    while (text_content[len])
-        len++;
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
-    w = write(fd, text_content, len);
-    if ( w != len)
-        close(fd);
-        return (-1);
-}
+	while (text_content[len])
+		len++;
 
-    close(fd);
-    return (1);
+	w = write(fd, text_content, len);
+	if (w != len)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
 }
