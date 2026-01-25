f55efbcad1b1ae725f5d98f30aa70891674f0c41
khadijaumud <kh.umud07@gmail.com>
2026-01-25 09:38:19 +0000

    fix 2nd task

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,17 +17,18 @@ int append_text_to_file(const char *filename, char *text_content)
     ssize_t w;
     int len = 0;
 
-    if filename == NULL
+    if (filename == NULL)
         return (-1);
 
     fd = open(filename, O_WRONLY | O_APPEND);
     if (fd == -1)
         return (-1);
 
-    if text_content == NULL
+    if (text_content == NULL)
+    {
         close(fd);
         return (1);
-
+    }
     while (text_content[len])
         len++;
 
