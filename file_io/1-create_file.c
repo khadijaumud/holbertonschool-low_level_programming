34e70fcc85bf8f4943c66af968f4451e38fd20e6
khadijaumud <kh.umud07@gmail.com>
2026-01-25 08:54:06 +0000

    add 1-create_file.c task

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file and writes text to it
+ * @filename: name of the file to create
+ * @text_content: string to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		w = write(fd, text_content, len);
+		if (w != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
